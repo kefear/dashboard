ffd204a61850b0dd7e8e2befc94fb233512588d0
Yevgeniy Gavrilov <y@gavrilov.kz>
2021-06-20 18:05:12 +0600

    Dashboard v1
    1. reverted back to head

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <?php
-        require_once("/blocks/head.php");
-    ?>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TPT6GSB');</script>
+    <!-- End Google Tag Manager -->
+    <title>Дэшборд отдела разработки Мечта</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap" rel="stylesheet">
+    <link href="css/normalize.min.css" rel="stylesheet" media="all">
+    <link href="css/theme-v1.css" rel="stylesheet" media="all">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
     <!-- Google Tag Manager (noscript) -->
