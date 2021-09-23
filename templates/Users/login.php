738541a5a0dcb416f69661f9c3a1b10f3986ebd0
Yevgeniy Gavrilov <y@gavrilov.kz>
2021-09-24 01:08:14 +0600

    Testing

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -1,8 +1,3 @@
-<?php
-/**
- * @var \App\View\AppView $this
- */
-?>
 <div class="users form content">
     <a href="https://oauth.yandex.ru/authorize?response_type=token&client_id=7dcb03416bd2465c89cda186f5a2fe6b" class="button"><?= __('Login with Yandex') ?></a>
 </div>
