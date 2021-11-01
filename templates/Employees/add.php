e738a07575f6d72a43612c7e7d2905b820098af2
Yevgeniy Gavrilov <y@gavrilov.kz>
2021-11-01 17:39:27 +0600

    Employee
    1. added an option for empty report field

--- templates/Employees/add.php
+++ templates/Employees/add.php
@@ -26,7 +26,7 @@
                     echo $this->Form->control('phone');
                     echo $this->Form->control('team_id', ['options' => $teams, 'empty' => true]);
                     echo $this->Form->control('role_id', ['options' => $roles, 'empty' => true]);
-                    echo $this->Form->control('report_id');
+                    echo $this->Form->control('report_id', ['empty' => true]);
                     echo $this->Form->control('salary');
                     echo $this->Form->control('status_id', ['options' => $statuses]);
                 ?>
