da0380b816cba503478ca425ed83770256f869fd
Yevgeniy Gavrilov <y@gavrilov.kz>
2021-11-24 17:57:14 +0600

    Migration
    1. users added new column to store telegram handle

---
+++ config/Migrations/20211124115658_UserNewColumnTelegram.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class UserNewColumnTelegram extends AbstractMigration
+{
+    /**
+     * Up Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-up-method
+     * @return void
+     */
+    public function up()
+    {
+
+        $this->table('employees')
+            ->addColumn('telegram', 'text', [
+                'after' => 'status_id',
+                'default' => null,
+                'length' => null,
+                'null' => true,
+            ])
+            ->update();
+    }
+
+    /**
+     * Down Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-down-method
+     * @return void
+     */
+    public function down()
+    {
+
+        $this->table('employees')
+            ->removeColumn('telegram')
+            ->update();
+    }
+}
